f8ce3962b7784786204d84e3eb2de4ffe468f657
calderon <hugo.velasquez.le@gmail.com>
2020-05-22 16:42:08 +1000

    Added KPI-Ordenes-de-Almacen-no-entregadas.sql

---
+++ KPIs/Ventas/Ventas-Operacion/KPI-Ordenes-de-Almacen-no-entregadas.sql
@@ -0,0 +1,17 @@
+SELECT shipdate , count(WM_InoutBound_ID) 
+FROM WM_InoutBound iob
+WHERE iob.shipdate >= (Current_Date- {{Dias_previos}} )
+AND iob.IsSOTrx='Y' 
+AND iob.Processed='Y'
+AND iob.AD_Client_ID = 1000000
+AND iob.wm_InoutBound_ID IN  (
+    SELECT distinct iobl.wm_Inoutbound_ID
+	FROM WM_InoutBoundLine iobl
+	INNER JOIN C_OrderLine ol ON (ol.C_OrderLine_ID= iobl.C_OrderLine_ID)
+	LEFT JOIN m_InoutLine iol ON (iol.C_OrderLine_ID = iobl.C_OrderLine_ID)
+	WHERE iob.WM_InoutBound_ID=iobl.WM_InoutBound_ID
+    AND ol.QtyOrdered <> ol.QtyDelivered
+	AND coalesce(iobl.PickedQty,0) < coalesce(iol.MovementQty,0)
+                            )
+GROUP BY iob.shipdate 
+ORDER BY iob.shipdate
