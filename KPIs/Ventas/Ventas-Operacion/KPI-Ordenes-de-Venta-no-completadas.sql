a7b2b3903785e127ea6cce106a931d483ef6f397
calderon <hugo.velasquez.le@gmail.com>
2020-05-22 17:07:52 +1000

    Added KPI-Ordenes-de-Venta-no-completadas.sql

---
+++ KPIs/Ventas/Ventas-Operacion/KPI-Ordenes-de-Venta-no-completadas.sql
@@ -0,0 +1,14 @@
+SELECT ord.dateordered , count(C_Order_ID) 
+FROM C_Order ord
+WHERE ord.dateordered >= (Current_Date- {{Dias_previos}} )
+AND ord.issotrx = 'Y' 
+AND ord.docstatus in ('DR','IP','IV') 
+AND EXISTS(
+    SELECT 1 
+    FROM C_DocType dt 
+    WHERE dt.C_DocType_ID = ord.C_DocTypeTarget_ID 
+    AND dt.DocBaseType = 'SOO' AND (dt.DocSubTypeSO IS NULL OR dt.DocSubTypeSO <> 'RM')
+            )
+AND ord.AD_Client_ID = 1000000
+GROUP BY ord.dateordered 
+ORDER BY ord.dateordered
