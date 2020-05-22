b33827c82e602a7ff5d47d55a1fea4cf030266fd
calderon <hugo.velasquez.le@gmail.com>
2020-05-22 16:57:26 +1000

    Added KPI-Entregas-no-completadas.sql

---
+++ KPIs/Ventas/Ventas-Operacion/KPI-Entregas-no-completadas.sql
@@ -0,0 +1,9 @@
+SELECT movementdate , count(*) 
+FROM  M_InOut
+WHERE movementdate  >=  Current_Date- {{Dias_previos}} 
+AND issotrx = 'Y' 
+AND m_Inout.docstatus IN ('DR','IP','IN') 
+AND movementtype='C-'
+AND AD_Client_ID=1000000
+GROUP BY movementdate 
+ORDER BY movementdate
