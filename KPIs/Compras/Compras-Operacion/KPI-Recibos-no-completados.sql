f4b205ba82645a833424f888fad41d72cb4b3b5d
calderon <hugo.velasquez.le@gmail.com>
2020-05-22 16:50:12 +1000

    Added KPI-Recibos-no-completados.sql

---
+++ KPIs/Compras/Compras-Operacion/KPI-Recibos-no-completados.sql
@@ -0,0 +1,12 @@
+SELECT movementdate, count(*)
+FROM M_Inout
+WHERE movementdate >= TO_DATE('2020/01/01','yyyy/MM/dd') 
+AND issotrx = 'N' 
+AND docstatus IN ('DR','IP','IV')
+AND AD_Client_ID = 1000000
+GROUP BY movementdate
+ORDER BY movementdate
+
+
+
+
