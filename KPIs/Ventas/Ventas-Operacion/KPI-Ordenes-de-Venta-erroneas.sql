5247b02f0dbd4d2a882e51448b6b9757481d573b
calderon <hugo.velasquez.le@gmail.com>
2020-05-22 17:05:00 +1000

    Added KPI-Ordenes-de-Venta-erroneas.sql

---
+++ KPIs/Ventas/Ventas-Operacion/KPI-Ordenes-de-Venta-erroneas.sql
@@ -0,0 +1,18 @@
+SELECT ord.dateordered , count(ord.C_Order_ID) 
+FROM C_Order ord
+WHERE  ord.dateordered >= (Current_Date- {{Dias_previos}} )
+AND ord.isSOTrx='Y'
+AND (  ord.DocStatus in ('IN') 
+        OR 
+       (ord.docstatus_rejectstatus = 'CL' and processed = 'N') 
+    ) 
+AND EXISTS(
+    SELECT 1 
+    FROM C_DocType dt 
+    WHERE dt.C_DocType_ID = ord.C_DocTypeTarget_ID 
+    AND dt.DocBaseType = 'SOO' 
+    AND (dt.DocSubTypeSO IS NULL OR dt.DocSubTypeSO <> 'RM')
+            )
+AND ord.AD_Client_ID = 1000000
+GROUP BY ord.dateordered 
+ORDER BY ord.dateordered
