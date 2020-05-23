877c1b71b1a76b591d2f9bd9158078840829914a
calderon <hugo.velasquez.le@gmail.com>
2020-05-23 15:59:45 +1000

    Added KPI-Pagos-no-Completados.sql

---
+++ KPIs/Tesoreria/Tesoreria-Operaciones/KPI-Pagos-no-Completados.sql
@@ -0,0 +1,13 @@
+SELECT p.datetrx, count(p.C_Payment_ID) 
+FROM C_Payment p
+WHERE  p.datetrx >= (Current_Date - {{Dias_previos}} )
+AND p.docstatus in ('DR','IP','IV') and p.isreceipt = 'N'
+AND p.C_BPartner_ID IS NOT NULL 
+AND EXISTS(SELECT 1 
+           FROM C_BankAccount ba 
+           INNER JOIN C_Bank b ON(b.C_Bank_ID = ba.C_Bank_ID) 
+           WHERE ba.C_BankAccount_ID = p.C_BankAccount_ID 
+           AND b.BankType = 'B')
+AND p.AD_Client_ID = 1000000
+GROUP BY p.datetrx 
+ORDER BY p.datetrx
