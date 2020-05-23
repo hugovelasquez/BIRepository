5bb3eaea826f1c8efd1e4649fd3184f4e09734fc
calderon <hugo.velasquez.le@gmail.com>
2020-05-23 15:31:17 +1000

    Added KPI-Gastos-de-Retaceo-no-distribuidos.sql

---
+++ KPIs/Compras/Compras-Operacion/KPI-Gastos-de-Retaceo-no-distribuidos.sql
@@ -0,0 +1,14 @@
+SELECT inv.dateinvoiced , count(inv.C_Invoice_ID) 
+FROM C_Invoice inv
+WHERE inv.dateinvoiced >= (Current_Date- {{Dias_previos}} )
+AND inv.IsSoTrx='N' 
+AND inv.docstatus IN ('CO')
+AND inv.AD_Client_ID = 1000000
+AND inv.c_Invoice_ID IN (
+    SELECT ivl.C_Invoice_ID FROM c_Invoiceline ivl
+	LEFT JOIN c_Landedcostallocation lca ON ivl.c_Invoiceline_ID=lca.c_Invoiceline_ID
+	where ivl.m_Product_ID IS NULL AND ivl.c_Charge_ID <> 5000001 
+	AND lca.c_Invoiceline_ID IS NULL AND ivl.user4_ID IS NOT NULL
+                        )
+GROUP BY inv.dateinvoiced 
+ORDER BY inv.dateinvoiced
