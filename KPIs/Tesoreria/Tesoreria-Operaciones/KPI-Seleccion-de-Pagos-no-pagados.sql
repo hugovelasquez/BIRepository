574adffd8c0ec448f78f177e48d95ceeed417b13
calderon <hugo.velasquez.le@gmail.com>
2020-05-23 15:54:43 +1000

    Added KPI-Seleccion-de-Pagos-no-pagados.sql

---
+++ KPIs/Tesoreria/Tesoreria-Operaciones/KPI-Seleccion-de-Pagos-no-pagados.sql
@@ -0,0 +1,16 @@
+SELECT ps.paydate, count(ps.C_PaySelection_ID) 
+FROM C_PaySelection ps
+WHERE  ps.paydate >= (Current_Date - {{Dias_previos}} )
+
+AND  ps.DocStatus = 'CO' 
+AND ps.C_BankAccount_ID IS NOT NULL 
+AND EXISTS(
+    SELECT 1 
+    FROM C_PaySelectionCheck psc 
+	LEFT JOIN C_Payment p ON (p.C_Payment_ID = psc.C_Payment_ID) 
+	WHERE psc.C_PaySelection_ID = ps.C_PaySelection_ID 
+	AND (psc.C_Payment_ID IS NULL OR p.DocStatus NOT IN('CO', 'CL'))
+        )
+AND ps.AD_Client_ID = 1000000
+GROUP BY ps.paydate 
+ORDER BY ps.paydate
