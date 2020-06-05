6bce6954ce1fb5789105b48cccdacac7501f9a4e
calderon <hugo.velasquez.le@gmail.com>
2020-06-05 11:30:56 +1000

    Deleted old charts and added new charts into Ventas-Resultados

--- KPIs/Ventas/Ventas-Resultados/KPI-Facturacion-Vendedores.sql
+++
@@ -1,6 +0,0 @@
-SELECT ild.salesrepname, sum(ild.linenetamtreal) as facturado, sum(ild.linenetamtvoided) as anulaciones, sum(ild.linenetamtreturned) as devoluciones
-FROM rv_invoiceline_detail ild
-WHERE ild.dateinvoiced BETWEEN {{Fecha_inicio}} AND {{Fecha_final}}
-GROUP BY ild.salesrepname
-ORDER BY sum(ild.linenetamtreal) desc
-LIMIT  {{Limite}}
