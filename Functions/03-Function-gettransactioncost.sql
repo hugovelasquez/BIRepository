2f586b6d99f60564a54285ae5f676d2e6d155576
Mario Calderon - Systemhaus Westfalia <mario.calderon@westfalia-it.com>
2020-06-12 08:37:18 -0600

    Cost value=0 if there is no result.

--- Functions/03-Function-gettransactioncost.sql
+++ Functions/03-Function-gettransactioncost.sql
@@ -1,30 +1,31 @@
 -- product cost out of CostDetail for a given invoiceline
 
 -- Function: gettransactioncost(numeric)
+
 -- DROP FUNCTION gettransactioncost(numeric);
 
 CREATE OR REPLACE FUNCTION gettransactioncost(p_c_invoiceline_id numeric)
   RETURNS numeric AS
 $BODY$
-
 DECLARE
-	v_Cost						NUMERIC;
-	v_LinenetAmt				NUMERIC;
-	v_DocStatus					character varying;
-
+	v_Cost		 NUMERIC;
+	v_LinenetAmt NUMERIC;
+	v_DocStatus	 character varying;
 BEGIN
+v_Cost = 0.0;
 
-select i.docstatus, sum(ivl.linenetamt) , coalesce(sum(cd.costamt),0) INTO v_DocStatus, v_LinenetAmt, v_Cost
+SELECT i.docstatus, sum(ivl.linenetamt) , coalesce(sum(cd.costamt),0) INTO v_DocStatus, v_LinenetAmt, v_Cost
+FROM m_Costdetail cd 
+INNER JOIN m_InoutLine iol ON cd.m_INoutline_ID = iol.m_Inoutline_ID
+INNER JOIN c_invoiceline ivl ON iol.c_Orderline_ID=ivl.c_orderline_ID 
+INNER JOIN C_Invoice i ON (ivl.c_Invoice_ID=i.c_Invoice_ID)
+WHERE ivl.c_INvoiceline_ID = p_C_InvoiceLine_ID
+GROUP BY i.docstatus;
 
-from m_Costdetail cd 
-INNER JOIN m_InoutLine iol on cd.m_INoutline_ID = iol.m_Inoutline_ID
-INNER JOIN c_invoiceline ivl on iol.c_Orderline_ID=ivl.c_orderline_ID 
-INNER JOIN C_Invoice i on (ivl.c_Invoice_ID=i.c_Invoice_ID)
-where
-ivl.c_INvoiceline_ID = p_C_InvoiceLine_ID
-group by i.docstatus;
+IF (v_DocStatus NOT IN ('CL', 'CO') AND v_LinenetAmt = 0) 
+THEN  v_Cost = 0 ; 
+END IF;
 
-IF (v_DocStatus not in ( 'CL', 'CO') AND v_LinenetAmt = 0) THEN  v_Cost = 0 ; END IF;
 RETURN round(v_Cost,4);
 
 END;
