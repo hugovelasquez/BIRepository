405bd6e584b9d2dc127eca8f6b84d1526655258f
calderon <hugo.velasquez.le@gmail.com>
2020-07-01 17:06:04 +1000

    Added functions for KPI return on non current assets

---
+++ KPIs/Financieros/Performance/KPI-Return_on_Non_Current_Assets2.sql
@@ -0,0 +1,27 @@
+-- KPI- Return on Non-Current Assets2 
+SELECT 
+to_char(date_part('YEAR'::text, now()::timestamp) - 4, '9999') as "Año",
+get_kpi_return_on_non_current_assets2(1000000, 1000002, date_part('YEAR'::text, now()::timestamp) - 4, 'A') as "Return on Non-Current Assets2 (%)"
+
+UNION
+SELECT 
+to_char(date_part('YEAR'::text, now()::timestamp) - 3, '9999') as "Año",
+get_kpi_return_on_non_current_assets2(1000000, 1000002, date_part('YEAR'::text, now()::timestamp) - 3, 'A') as "Return on Non-Current Assets2 (%)"
+
+UNION
+SELECT 
+to_char(date_part('YEAR'::text, now()::timestamp) - 2, '9999') as "Año",
+get_kpi_return_on_non_current_assets2(1000000, 1000002, date_part('YEAR'::text, now()::timestamp) - 2, 'A') as "Return on Non-Current Assets2 (%)"
+
+UNION
+SELECT 
+to_char(date_part('YEAR'::text, now()::timestamp) - 1, '9999') as "Año",
+get_kpi_return_on_non_current_assets2(1000000, 1000002, date_part('YEAR'::text, now()::timestamp) - 1, 'A') as "Return on Non-Current Assets2 (%)"
+
+UNION
+SELECT 
+to_char(date_part('YEAR'::text, now()::timestamp) - 0, '9999') as "Año",
+get_kpi_return_on_non_current_assets2(1000000, 1000002, date_part('YEAR'::text, now()::timestamp) - 0, 'A') as "Return on Non-Current Assets2 (%)"
+
+ORDER BY 1
+
